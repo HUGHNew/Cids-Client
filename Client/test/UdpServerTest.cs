be349134f97c6fe0c683246c4430f606998fd8a7
HUGHNew <1728902978@qq.com>
2021-05-19 13:19:08 +0800

    [:hammer:]Client Test written

--- Client/test/UdpServerTest.cs
+++ Client/test/UdpServerTest.cs
@@ -9,34 +9,36 @@ namespace Client
     {
         public static void ServerOn(String Ip="127.0.0.1",int port=20800)
         {
-            UdpClient Server = new UdpClient(Ip, port);
             byte[] data = new byte[1024];
-            IPEndPoint ipep = new IPEndPoint(IPAddress.Any, 9050);
+            IPEndPoint ipep = new IPEndPoint(IPAddress.Any, port);
             UdpClient newsock = new UdpClient(ipep);
 
-            Console.WriteLine("Waiting for a client...");
-
             IPEndPoint sender = new IPEndPoint(IPAddress.Any, 0);
+            Console.WriteLine("Waiting for a client...");
+            RecvMain(ref newsock,ref sender);
 
-            data = newsock.Receive(ref sender);
-
-            Console.WriteLine("Message received from {0}:", sender.ToString());
-            Console.WriteLine(Encoding.ASCII.GetString(data, 0, data.Length));
-
-            string welcome = "Welcome to my test server";
-            data = Encoding.ASCII.GetBytes(welcome);
-            newsock.Send(data, data.Length, sender);
-
-            while (true)
-            {
-                data = newsock.Receive(ref sender);
-
-                Console.WriteLine(Encoding.ASCII.GetString(data, 0, data.Length));
-                newsock.Send(data, data.Length, sender);
-            }
+            RecvMirror(ref newsock, ref sender);
         }
-        public static void RecvMain(ref UdpClient udp) { 
-            
+        public static void RecvMain(ref UdpClient udp,ref IPEndPoint sender) {
+            byte[] data;
+            data = udp.Receive(ref sender); // request for mirror
+            byte[] loop = { 127, 0, 0, 1 };
+            Console.WriteLine("Message received from {0}:", sender.ToString());
+            //for(int i = 0; i < 10; ++i)
+            //{
+            //}
+                udp.Send(loop, 4, sender);
+        }
+        public static void RecvMirror(ref UdpClient udp, ref IPEndPoint sender)
+        {
+            byte[] data=null;
+            while (data==null||data?.Length > 20)
+            {
+                data = udp.Receive(ref sender);
+            }
+            string json = System.IO.File.ReadAllText("../../test/json/noUrl.json");
+            data = Encoding.UTF8.GetBytes(json);
+            udp.Send(data,data.Length,sender);
         }
     }
 }
